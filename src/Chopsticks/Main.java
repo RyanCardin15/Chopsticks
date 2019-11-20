f32a6e7e8850bfe8041730a702463a82e241ceaa
danielr9173 <danielr9173@gmail.com>
2019-11-20 15:31:24 -0600

    Start Menu was created!

--- src/Chopsticks/Main.java
+++ src/Chopsticks/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Game_2p.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("StartingMenu.fxml"));
         primaryStage.setTitle("Chopsticks");
-        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.centerOnScreen();
         primaryStage.setResizable(false);
         primaryStage.show();
     }
---
+++ src/Chopsticks/StartingMenu.java
@@ -0,0 +1,47 @@
+package Chopsticks;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartingMenu {
+    @FXML
+    private CheckBox onePlayer,twoPlayer;
+    @FXML
+    private Button traditional;
+
+    public void change_page(Button b, String page) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(page));
+            Stage stage = (Stage)b.getScene().getWindow();
+            stage.setScene(new Scene(root));stage.centerOnScreen();
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startGame() {
+
+        if (onePlayer.isSelected() && !twoPlayer.isSelected()) {
+            System.out.println("Yea");
+            if (traditional.isPressed()) {
+                System.out.println("Yea");
+                change_page(traditional, "Game_1p.fxml");
+            }
+        }
+        else if (twoPlayer.isSelected() && !onePlayer.isSelected()) {
+            System.out.println("Yea");
+            if (traditional.isPressed()) {
+                System.out.println("Yea");
+                change_page(traditional, "Game_2p.fxml");
+            }
+        }
+    }
+}
